812270a043453c3c4327789eda4d2bd5431100dc
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2023-06-09 23:00:41 +0530

    Open for extension but closed for modification

---
+++ 2. open close/right.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+
+class invoiceDao {
+    public : 
+        virtual void save() {}    
+};
+
+class invoiceDaoDB : public invoiceDao {
+    public : 
+        void save() {
+            /* Save to DB */
+        }
+};
+
+class invoiceDaoFile : public invoiceDao {
+    public : 
+        void save() {
+            /* Save to FILE */
+        }
+};
+
+/*
+    this is the correct way to write extensible code
+*/
+
+int main() {}
